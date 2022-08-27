0607028b49de858618239c1684e591065dda6fb4
JorgeBanon <103133562+JorgeBanon@users.noreply.github.com>
2022-08-27 20:31:22 +0200

    Add files via upload

---
+++ numbers.js
@@ -0,0 +1,7 @@
+let estatura_cm = 172;
+let estatura_m = 1.72;
+let peso_kg = 65.5;
+console.log(Math.round(estatura_m));
+console.log(Math.floor(peso_kg));
+let max_value = Number.MAX_VALUE;
+console.log(max_value + 1)
